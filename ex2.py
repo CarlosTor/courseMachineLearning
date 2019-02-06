07d0ee078ff4ec92b10d5d886f80f5438b215d70
CarlosTor <carlostordiez@gmail.com>
2019-02-06 22:01:04 +0100

    Exercise 2 completed

--- ex2.py
+++ ex2.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import math as m
 import matplotlib.pyplot as plt
-from random import randint
+from scipy import optimize
 
 
 ####PART1####
@@ -20,11 +19,18 @@ def part1():
 
 ####PART2####
 def costFunction(X,y,theta):
+    return computeCost(theta,X,y),computeGradient(X,y,theta)
+
+def computeCost(theta,X,y):
     minv = 1./len(y)
     h = sigmoid(theta.dot(X.T))
     cost = minv * (- y.dot(np.log(h)) - (1.- y).dot(np.log(1.- h)))
-    gradient = minv * (h-y).dot(X)
-    return cost, gradient
+    return cost
+
+def computeGradient(X,y,theta):
+    minv = 1./len(y)
+    h = sigmoid(theta.dot(X.T))
+    return minv * (h-y).dot(X)
 
 def part2():
     data = np.loadtxt('data/ex2data1.txt',delimiter=',',unpack=True).T
@@ -38,6 +44,55 @@ def part2():
 ########
 
 
+####PART3####
+def findOptimalTheta(X,y,theta):
+    result = optimize.fmin( computeCost, x0=theta, args=(X,y), maxiter=400, full_output=True )
+    theta = result[0]
+    cost = result[1]
+    return theta,cost
+
+def part3():
+    data = np.loadtxt('data/ex2data1.txt',delimiter=',',unpack=True).T
+    X = np.c_[np.ones((data.shape[0],1)) ,data[:,:-1]]
+    y = data[:,-1]
+    n = X.shape[1]
+    theta = np.zeros(n)
+
+    theta,cost = findOptimalTheta(X,y,theta)
+    print 'With optimal theta = '+str(theta)+' --> cost = '+str(cost)
+
+    plt.figure()
+    plt.scatter((X[y==0,1]),(X[y==0,2]),marker='o',color='y',label='Not admitted')
+    plt.scatter((X[y==1,1]),(X[y==1,2]),marker='x',color='b',label='Admitted')
+    plt.plot( np.array([min(X[:,1]),max(X[:,1])]) , (-1./theta[2])*(theta[1]*np.array([min(X[:,1]),max(X[:,1])])+theta[0]) )
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+    plt.xlim([25, 115])
+    plt.ylim([25, 115])
+    plt.legend()
+    plt.show()
+########
+
+####PART4####
+def predict(X,y,test,theta):
+    theta = findOptimalTheta(X,y,theta)[0]
+    prob = sigmoid(test.dot(theta))
+    return 1 if prob>=0.5 else 0,theta
+
+def part4():
+    data = np.loadtxt('data/ex2data1.txt',delimiter=',',unpack=True).T
+    X = np.c_[np.ones((data.shape[0],1)) ,data[:,:-1]]
+    y = data[:,-1]
+    n = X.shape[1]
+    theta = np.zeros(n)
+
+    test=np.array([1.,45.,85.])
+    print 'The prediction for '+str(test[1:])+' is '+str(predict(X,y,test,theta)[0])+' (probability = '+str(sigmoid(test.dot(predict(X,y,test,theta)[1])))+')'
+
+########
+
 if __name__ == '__main__':
     part1()
     part2()
+    part3()
+    part4()
